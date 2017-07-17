032f5e04445cdcabcdaaa42f61c159f367dd768a
Hema M <hemamurugesanece@gmail.com>
2017-07-17 10:27:30 +0530

    RepeatedNum.java

---
+++ RepeatedNum.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+class RepeatedNum
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n-1;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(a[i]==a[j])
+{
+System.out.println(a[j]);
+}
+}
+}
+}
+}
